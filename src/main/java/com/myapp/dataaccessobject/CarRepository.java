a1d98e7d4a1256887cd57d29d11539fcea01d2d9
Prashant Suri <PrashantSuri@Prashants-MBP.attlocal.net>
2021-01-23 21:36:39 -0800

    feature1 sample comment change

--- src/main/java/com/myapp/dataaccessobject/CarRepository.java
+++ src/main/java/com/myapp/dataaccessobject/CarRepository.java
@@ -3,6 +3,11 @@ package com.myapp.dataaccessobject;
 import com.myapp.domainobject.CarDO;
 import org.springframework.data.repository.CrudRepository;
 
+/**
+ * Database Access Object for car table.
+ * feature1 sample comment
+ */
+
 public interface CarRepository extends CrudRepository<CarDO, String> {
 
     CarDO findByLicensePlate(String licensePlate);
